4c8e7cd75c65e0c6b3b6f08c41230c5ff1de68ed
Tamby Mampionona <tambymampioninaheriharson@gmail.com>
2020-08-28 09:30:51 +0300

    (Debut-Jsx)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './App.css'
+import GuessCount from './GuessCount'
+import Card from './Card'
+
+class App extends Component {
+  handleCardClick(card){
+    console.log(card, 'clicked')
+  }
+  render(){
+    const won = new Date().getSeconds() % 2 === 0
+    return (
+    <div className = "memory">
+      <GuessCount guesses={0} />
+      <Card card="😁" feedback="hidden" onClick={this.handleCardClick} />
+      <Card card="❤" feedback="justMatched" onClick={this.handleCardClick} />
+      <Card card="😎" feedback="justMismatched" onClick={this.handleCardClick} />
+      <Card card="😭" feedback="visible" onClick={this.handleCardClick} />
+      <Card card="🚑" feedback="hidden" onClick={this.handleCardClick} />
+      {won && <p>Gagne ! </p> }
     </div>
-  );
+    )
+  }
 }
-
-export default App;
+export default App
