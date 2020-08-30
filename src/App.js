fb22fd05d097ef2700a21910c517ead92c12d93b
Tamby Mampionona <tambymampioninaheriharson@gmail.com>
2020-08-30 19:13:09 +0300

    (methode setState +++ )

--- src/App.js
+++ src/App.js
@@ -8,7 +8,13 @@ import HallOfFame, {FAKE_HOF} from './HallOfFame'
 const SIDE = 6
 const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
 class App extends Component {
-cards = this.generateCards()
+	state = {
+		cards: this.generateCards(),
+		currentPair: [],
+		guesses: 0,
+		matchedCardIndices: [],
+	}
+
 generateCards() {
 	const result = []
 	const size = SIDE * SIDE
@@ -19,16 +25,37 @@ generateCards() {
 }
 	return shuffle(result)
 }
-handleCardClick(card) {
-	console.log(card, 'clicked')
+getFeedbackForCard(index){
+	const { currentPair, matchedCardIndices } = this.state
+	const indexMatched = matchedCardIndices.includes(index)
+	if (currentPair.length < 2) {
+		return indexMatched || index === currentPair[0] ? 'visible' : 'hidden' 
+	}
+	if (currentPair.includes(index)) {
+		return indexMatched ? 'justMatched' : 'justMatched'
+	}
+	return indexMatched ? 'visible' : 'hidden'
+}
+	// Arrow fx for binding
+handleCardClick = index => {
+	const {currentPair} = this.state
+	if (currentPair.length === 2) {
+		return
+	}
+	if (currentPair.length === 0) {
+		this.setState({ currentPair: [index] })
+		return
+	}
+	this.handleNewPairClosedBy(index)
 }
 render() {
-	const won = new Date().getSeconds() % 2 === 0
+	const { cards, guesses, matchedCardIndices } = this.state
+	const won = matchedCardIndices.length === cards.length
 return (
 <div className="memory">
-	<GuessCount guesses={0} />
-	{this.cards.map((card,index) => (
-	<Card card={card} feedback="visible" key={index} onClick={this.handleCardClick} />
+	<GuessCount guesses={guesses} />
+	{cards.map((card,index) => (
+	<Card card={card} feedback={this.getFeedbackForCard(index)} key={index} index={index} onClick={this.handleCardClick} />
 	))}
 	{won && <HallOfFame entries={FAKE_HOF} /> }
 </div>
