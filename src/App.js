2ba209fecc0567eb8ced9701894941a485cd3b0b
Tamby Mampionona <tambymampioninaheriharson@gmail.com>
2020-09-07 21:30:56 +0300

    Version 01 de mon jeux Memory

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import HallOfFame, {FAKE_HOF} from './HallOfFame'
 
 const SIDE = 6
 const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
+const VISUAL_PAUSE_MSECS = 750
 class App extends Component {
 	state = {
 		cards: this.generateCards(),
@@ -47,6 +48,17 @@ handleCardClick = index => {
 		return
 	}
 	this.handleNewPairClosedBy(index)
+}
+handleNewPairClosedBy(index){
+	const {cards, currentPair, guesses, matchedCardIndices} = this.state
+	const newPair = [currentPair[0], index]
+	const newGuesses = guesses + 1
+	const matched = cards[newPair[0]] === cards[newPair[1]]
+	this.setState({ currentPair: newPair, guesses: newGuesses })
+	if (matched) {
+		this.setState({ matchedCardIndices: [...matchedCardIndices, ...newPair] })
+	}
+	setTimeout(() => this.setState({ currentPair: [] }), VISUAL_PAUSE_MSECS)
 }
 render() {
 	const { cards, guesses, matchedCardIndices } = this.state
